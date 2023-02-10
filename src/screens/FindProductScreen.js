0738f89ed886d9dc96e39f45024255d2b7cb4c9a
K2013928 <k2013928@kingston.ac.uk>
2023-02-10 12:21:21 +0000

    Shop floor map inputted

--- src/screens/FindProductScreen.js
+++ src/screens/FindProductScreen.js
@@ -1,18 +1,32 @@
-import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Image } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
 
+
 function FindProductScreen() {
-    return (
-        <View>
-            <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
-                <View>
-                <MaterialIcons style={styles.iconButton} name='settings' size={28}/>
-                </View>
-            </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+            <MaterialIcons style={styles.iconButton} name='settings' size={28}/>
+        </TouchableOpacity>
+        <View style={styles.image}>
+            <Image source={require('../../assets/shop-floor.png')} style={{width: 280, height: 280}}/>
         </View>
-    )
+    </View>
+   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignContent: 'center',
+    },
+    iconButton: {
+        padding: 5,
+        alignSelf:'flex-end',
+    },
+    image: {
+       alignSelf: 'center',
+    },
+});
 
 export default FindProductScreen;
